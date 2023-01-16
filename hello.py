34b98e8f93a1896025816b4f7bd1f60ce1882216
Garth Mortensen <mortensengarth@hotmail.com>
2023-01-16 16:48:52 -0600

    mess up formatting to test Black workflow

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-import logging
 import os
 
+
+import logging
+
 # logging tutorial; https://docs.python.org/3/howto/logging.html
-logging.basicConfig(
-    level=logging.DEBUG,
+logging.basicConfig(level=logging.DEBUG,
     format=" %(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y%m%d_%H%M%S",
-    handlers=[
-        logging.StreamHandler(),  # display log in console
+    handlers=[logging.StreamHandler(),  # display log in console
         logging.FileHandler(os.path.join("logs", "debug.log")),  # write log to file
     ],
 )
@@ -16,6 +16,7 @@ logging.basicConfig(
 logging.disable(logging.NOTSET)  # NOTSET < DEBUG < INFO < WARNING < ERROR < CRITICAL
 
 logging.info("program: started")
+
 
 
 def hello(name):
@@ -27,6 +28,9 @@ def hello(name):
 
 
 hello("pony")
+
+
+
 hello("goat")
 
 logging.info("program: completed")
